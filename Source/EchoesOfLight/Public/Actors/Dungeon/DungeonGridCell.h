a573145d325ba6dc97ea6e94c01f4171857e5d70
Kevin Rabe <Kevinrabe77@yahoo.com>
2023-09-20 20:10:08 -0500

    -dungeon grid cell class created

---
+++ Source/EchoesOfLight/Public/Actors/Dungeon/DungeonGridCell.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/GridCell.h"
+#include "DungeonGridCell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ECHOESOFLIGHT_API ADungeonGridCell : public AGridCell
+{
+	GENERATED_BODY()
+	
+};
